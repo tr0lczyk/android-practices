70461bff0c042d68d7907160cb4d208bffd2bf55
tr0lczyk <matt.olczyk@gmail.com>
2017-12-28 17:35:05 +0100

    preparation for app functionality

--- PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -10,36 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // PASTE CODE YOU WANT TO TEST HERE
+        int weekday = 5;
+        int weekend = 9;
+        int optimalHours = 7 * 8;
+        int actualHours = weekday;
+        actualHours = actualHours * 5 + weekend * 2;
+        int solution = optimalHours - actualHours;
+        display(solution);
 
     }
 
-    /**
-     * Display methods that allow the text to appear on the screen. Don't worry if you don't know
-     * how these work yet. We'll be covering them in lesson 3.
-     */
-
-    public void display(String text) {
+    public void display (int i) {
         TextView t = (TextView) findViewById(R.id.display_text_view);
-        t.setText(text);
-    }
-
-    public void display(int text) {
-        TextView t = (TextView) findViewById(R.id.display_text_view);
-        t.setText(text + "");
-    }
-
-    public void display1(String text) {
-        display(text);
-    }
-
-    public void display2(String text) {
-        TextView t = (TextView) findViewById(R.id.display_text_view_2);
-        t.setText(text);
-    }
-
-    public void display3(String text) {
-        TextView t = (TextView) findViewById(R.id.display_text_view_3);
-        t.setText(text);
+        t.setText(""+i);
     }
 }
