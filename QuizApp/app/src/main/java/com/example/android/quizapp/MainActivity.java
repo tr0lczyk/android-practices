0ddd811e5ff26a3fd9de91cac3e72ca9e19971ea
tr0lczyk <matt.olczyk@gmail.com>
2018-01-03 00:32:06 +0100

    java code operations - toast ready

--- QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public String playerName(){
+        EditText name = (EditText) findViewById(R.id.player_name);
+        String namePlayer = name.getText().toString();
+        return namePlayer;
+    }
+
+    public void displayToast(View view){
+        Toast.makeText(this, "Congratulations " + playerName() + "! 6/6 answers correct! The force is strong in you...", Toast.LENGTH_SHORT).show();
     }
 }
