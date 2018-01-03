4926ec9c08af0f505c3de86077d18f6cbaf56c4a
tr0lczyk <matt.olczyk@gmail.com>
2018-01-03 22:44:35 +0100

    styling the project

--- QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -16,97 +16,154 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public String playerName(){
+
+    /*
+    *
+    * @method that return's the player name
+    *
+    *
+    * */
+    public String playerName() {
         EditText name = (EditText) findViewById(R.id.player_name);
         String namePlayer = name.getText().toString();
         return namePlayer;
     }
 
-    public int questionOne(){
-        RadioButton buttonCorrect = (RadioButton) findViewById(R.id.one_good_answear);
-        boolean oneGoodAnswear = buttonCorrect.isChecked();
-        if (oneGoodAnswear) {
+    /*
+    *
+    * @method that checks if the player choose the answer for the first question correctly
+    *
+    * */
+    public int questionOne() {
+        RadioButton buttonCorrect = (RadioButton) findViewById(R.id.one_good_answer);
+        boolean oneGoodAnswer = buttonCorrect.isChecked();
+        if (oneGoodAnswer) {
             return 1;
         } else {
             return 0;
         }
     }
 
-    public int questionTwo(){
-        CheckBox checkBoxCorrectA = (CheckBox) findViewById(R.id.two_good_answearA);
-        boolean twoGoodAnswearA = checkBoxCorrectA.isChecked();
-        CheckBox checkBoxCorrectB = (CheckBox) findViewById(R.id.two_good_answearB);
-        boolean twoGoodAnswearB = checkBoxCorrectB.isChecked();
-        CheckBox checkBoxCorrectC = (CheckBox) findViewById(R.id.two_good_answearC);
-        boolean twoGoodAnswearC = checkBoxCorrectC.isChecked();
-        if (twoGoodAnswearA && twoGoodAnswearB && twoGoodAnswearC) {
+    /*
+    *
+    * @method that checks if the player choose the answer for the second question correctly
+    *
+    * */
+    public int questionTwo() {
+        CheckBox checkBoxCorrectA = (CheckBox) findViewById(R.id.two_good_answerA);
+        boolean twoGoodAnswerA = checkBoxCorrectA.isChecked();
+        CheckBox checkBoxCorrectB = (CheckBox) findViewById(R.id.two_good_answerB);
+        boolean twoGoodAnswerB = checkBoxCorrectB.isChecked();
+        CheckBox checkBoxCorrectC = (CheckBox) findViewById(R.id.two_good_answerC);
+        boolean twoGoodAnswerC = checkBoxCorrectC.isChecked();
+        if (twoGoodAnswerA && twoGoodAnswerB && twoGoodAnswerC) {
             return 1;
         } else {
             return 0;
         }
     }
 
-    public int questionThree(){
-        EditText shipNameCorrect = (EditText) findViewById(R.id.three_good_answear);
-        String threeGoodAnswear = shipNameCorrect.getText().toString().toLowerCase();
+    /*
+    *
+    * @method that checks if the player choose the answer for the third question correctly
+    *
+    * */
+    public int questionThree() {
+        EditText shipNameCorrect = (EditText) findViewById(R.id.three_good_answer);
+        String threeGoodAnswer = shipNameCorrect.getText().toString().toLowerCase();
         String falcon = "millenium falcon";
-        if (threeGoodAnswear.equals(falcon)) {
+        if (threeGoodAnswer.equals(falcon)) {
             return 1;
         } else {
             return 0;
         }
     }
 
-    public int questionFour(){
-        RadioButton buttonCorrect = (RadioButton) findViewById(R.id.four_good_answear);
-        boolean oneGoodAnswear = buttonCorrect.isChecked();
-        if (oneGoodAnswear) {
+    /*
+    *
+    * @method that checks if the player choose the answer for the fourth question correctly
+    *
+    * */
+    public int questionFour() {
+        RadioButton buttonCorrect = (RadioButton) findViewById(R.id.four_good_answer);
+        boolean oneGoodAnswer = buttonCorrect.isChecked();
+        if (oneGoodAnswer) {
             return 1;
         } else {
             return 0;
         }
     }
 
-    public int questionFive(){
-        CheckBox checkBoxCorrectA = (CheckBox) findViewById(R.id.five_good_answearA);
-        boolean fiveGoodAnswearA = checkBoxCorrectA.isChecked();
-        CheckBox checkBoxCorrectB = (CheckBox) findViewById(R.id.five_good_answearB);
-        boolean fiveGoodAnswearB = checkBoxCorrectB.isChecked();
-        CheckBox checkBoxCorrectC = (CheckBox) findViewById(R.id.five_good_answearC);
-        boolean fiveGoodAnswearC = checkBoxCorrectC.isChecked();
-        if (fiveGoodAnswearA && fiveGoodAnswearB && fiveGoodAnswearC) {
+    /*
+    *
+    * @method that checks if the player choose the answer for the fifth question correctly
+    *
+    * */
+    public int questionFive() {
+        CheckBox checkBoxCorrectA = (CheckBox) findViewById(R.id.five_good_answerA);
+        boolean fiveGoodAnswerA = checkBoxCorrectA.isChecked();
+        CheckBox checkBoxCorrectB = (CheckBox) findViewById(R.id.five_good_answerB);
+        boolean fiveGoodAnswerB = checkBoxCorrectB.isChecked();
+        if (fiveGoodAnswerA && fiveGoodAnswerB) {
             return 1;
         } else {
             return 0;
         }
     }
 
-    public int sumOfQuestions(int quest,int quest2, int quest3, int quest4, int quest5) {
-        int correctAnswears = 0;
-        correctAnswears += quest + quest2 + quest3 + quest4 + quest5;
-        return correctAnswears;
+    /*
+    *
+    * @method that checks total number of correct answers
+    *
+    * */
+    public int sumOfQuestions(int quest, int quest2, int quest3, int quest4, int quest5) {
+        int correctAnswers = 0;
+        correctAnswers += quest + quest2 + quest3 + quest4 + quest5;
+        return correctAnswers;
     }
 
-    public void displayToast(View view){
-        int sumOfAnswers = sumOfQuestions(questionOne(), questionTwo(),questionThree(), questionFour(), questionFive());
-        String answearZero = playerName() + ", do. Or do not. There is no try. Your score is " + sumOfAnswers + " points...";
-        String answearOne = playerName() + ", I have a bad feelings about this... " + sumOfAnswers + "/5 question answered correctly.";
-        String answearTwo = "Not bad " + playerName() + "! " + sumOfAnswers + "/5 points! In my experience there is no such thing as luck.";
-        String answearThree = "Nice, " + playerName() + "! Your score is " + sumOfAnswers + "/5! Stay on target.";
-        String answearFour = "Great " + playerName() + "! " + "but don’t get cocky. " + sumOfAnswers + "/5 questions answered correctly!";
-        String answearFive = "Marvelous " + playerName() +", your score is" + sumOfAnswers + "/5 ! The force is strong in you...";
-        if (sumOfAnswers == 0){
-            Toast.makeText(this, answearZero, Toast.LENGTH_LONG).show();
+
+    /*
+    *
+    * @method that checks if the player's name was applied
+    *
+    * */
+    public void isNameThere(View view){
+        String yourName = "Padawan, please state your name...";
+        if (playerName().equals("")) {
+            Toast.makeText(this, yourName, Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            displayToast();
+        }
+    }
+
+    /*
+    *
+    * @method that displays a different toast message, based on the number of correct answers
+    *
+    * */
+    public void displayToast() {
+        int sumOfAnswers = sumOfQuestions(questionOne(), questionTwo(), questionThree(), questionFour(), questionFive());
+        String answerZero = playerName() + ", do. Or do not. There is no try. Your score is " + sumOfAnswers + " points...";
+        String answerOne = playerName() + ", I have a bad feelings about this... " + sumOfAnswers + "/5 question answered correctly.";
+        String answerTwo = "Not bad " + playerName() + "! " + sumOfAnswers + "/5 points! In my experience there is no such thing as luck.";
+        String answerThree = "Nice, " + playerName() + "! Your score is " + sumOfAnswers + "/5! Stay on target.";
+        String answerFour = "Great " + playerName() + ", " + "but don’t get cocky. " + sumOfAnswers + "/5 questions answered correctly!";
+        String answerFive = "Marvelous " + playerName() + ", your score is" + sumOfAnswers + "/5 ! The force is strong in you...";
+        if (sumOfAnswers == 0) {
+            Toast.makeText(this, answerZero, Toast.LENGTH_LONG).show();
         } else if (sumOfAnswers == 1) {
-            Toast.makeText(this, answearOne, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, answerOne, Toast.LENGTH_LONG).show();
         } else if (sumOfAnswers == 2) {
-            Toast.makeText(this, answearTwo, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, answerTwo, Toast.LENGTH_LONG).show();
         } else if (sumOfAnswers == 3) {
-            Toast.makeText(this, answearThree, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, answerThree, Toast.LENGTH_LONG).show();
         } else if (sumOfAnswers == 4) {
-            Toast.makeText(this, answearFour, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, answerFour, Toast.LENGTH_LONG).show();
         } else if (sumOfAnswers == 5) {
-            Toast.makeText(this, answearFive, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, answerFive, Toast.LENGTH_LONG).show();
         }
     }
 }
+
