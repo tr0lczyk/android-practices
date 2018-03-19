f1bbd409efb70a6783688961a2ae64344b05dfff
tr0lczyk <matt.olczyk@gmail.com>
2018-03-19 23:27:20 +0100

    mediaPlayer practice

--- MusicPlayer2/app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ MusicPlayer2/app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,16 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        double startTime = 0;
         final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.skyrim_sound);
 
         Button playButton = findViewById(R.id.buttonPlay);
-        Button pauseButton = findViewById(R.id.buttonPause);
+        final Button pauseButton = findViewById(R.id.buttonPause);
         Button stopButton = findViewById(R.id.buttonStop);
+        pauseButton.setEnabled(false);
+        SeekBar seekBar = findViewById(R.id.seekBar);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mediaPlayer.start();
+                pauseButton.setEnabled(true);
             }
         });
 
