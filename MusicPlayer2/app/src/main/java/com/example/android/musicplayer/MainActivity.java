f03cb787d7289d15167bfb3e2ecbf929fd3fc09c
tr0lczyk <matt.olczyk@gmail.com>
2018-03-19 23:01:48 +0100

    create mediaPlayer environment

--- MusicPlayer2/app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ MusicPlayer2/app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.skyrim_sound);
+
+        Button playButton = findViewById(R.id.buttonPlay);
+        Button pauseButton = findViewById(R.id.buttonPause);
+        Button stopButton = findViewById(R.id.buttonStop);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.reset();
+            }
+        });
     }
 }
